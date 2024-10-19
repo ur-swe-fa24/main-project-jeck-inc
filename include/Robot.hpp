a53b817e03ae99882250ad3a52b992d7ece29b3a
Kritim708 <105226726+Kritim708@users.noreply.github.com>
2024-10-19 12:35:20 -0400

    Changes on the main files and robot files after creating cmake

--- include/Robot.hpp
+++ include/Robot.hpp
@@ -6,28 +6,25 @@
 
 namespace robot {
 
-// Enum class for size
-enum class Size {
-    Large,
-    Medium,
-    Small
-};
-
-// Enum class for function
-enum class Function {
-    Scrub,
-    Vacuum,
-    Shampoo
-};
 
 class Robot {
-private:
-    static int nextId; // Static variable to hold the next robot ID
-    int id;            // Robot ID
-    Function task;    // Robot task
-    Size size;        // Robot size
 
 public:
+    
+    // Enum class for size
+    enum class Size {
+        Large,
+        Medium,
+        Small
+    };
+
+    // Enum class for function
+    enum class Function {
+        Scrub,
+        Vacuum,
+        Shampoo
+    };
+
     // Constructor
     Robot(Function task, Size size);
 
@@ -35,6 +32,12 @@ public:
     int getId() const;
     Function getTask() const;
     Size getSize() const;
+
+private:
+    static int nextId; // Static variable to hold the next robot ID
+    int id;            // Robot ID
+    Function task;    // Robot task
+    Size size;        // Robot size
 };
 
 } // namespace robot
